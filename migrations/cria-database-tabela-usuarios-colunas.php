68079090b7cf529e0865f7e036769ccc70199e5a
lcarlos <luizsilvatraders@hotmail.com>
2023-07-24 15:02:06 -0300

    Adicina arquivo docker e arquivos migrations no projeto

---
+++ migrations/cria-database-tabela-usuarios-colunas.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+    CREATE DATABASE db_cassum;
+
+    USE db_cassum;
+
+    CREATE TABLE usuarios (
+        Id INT AUTO_INCREMENT PRIMARY KEY,
+        Nome VARCHAR(255) NOT NULL,
+        Usuario VARCHAR(100) NOT NULL,
+        Senha VARCHAR(255) NOT NULL,
+        Criado_em DATETIME NOT NULL,
+        Atualizado_em DATETIME NOT NULL
+    );
+
+*/
---
+++ migrations/inseri-10-usuarios-na-tabela-usuarios.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+
+    INSERT INTO usuarios (Nome, Usuario, Senha, Criado_em, Atualizado_em) VALUES
+    ('Luiz Silva', 'luiz_silva', 'luizsilva', NOW(), NOW()),
+    ('Tamires mourao', 'tamires_mourao', 'tamiresmourao', NOW(), NOW()),
+    ('Pedro Almeida', 'pedro_almeida', 'pedroalmeida', NOW(), NOW()),
+    ('Ana Oliveira', 'ana_oliveira', 'anaoliveira', NOW(), NOW()),
+    ('Lucas Pereira', 'lucas_pereira', 'lucaspereira', NOW(), NOW()),
+    ('Carla Souza', 'carla_souza', 'carlasouza', NOW(), NOW()),
+    ('Marcos Santos', 'marcos_santos', 'marcossantos', NOW(), NOW()),
+    ('Mariana Lima', 'mariana_lima', 'marianalima', NOW(), NOW()),
+    ('Gustavo Rodrigues', 'gustavo_rodrigues', 'gustavorodrigues', NOW(), NOW()),
+    ('Laura Fernandes', 'laura_fernandes', 'laurafernandes', NOW(), NOW());
+
+    */
